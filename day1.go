167f03a73f707051c942766e5ceb18a6e3ec831f
Jory Geerts <jory@jorygeerts.nl>
2018-12-04 23:07:43 +0100

    Day One

--- day1.go
+++ day1.go
@@ -1,5 +1,50 @@
 package aoc2018
 
-func DayOneTaskOne() string {
-	return "lala"
+import (
+	"strconv"
+	"strings"
+)
+
+func DayOneTaskOne(changes string) int {
+	steps := strings.Split(changes, ", ")
+	result := 0
+
+	for _, step := range steps {
+		stepChange, err := strconv.Atoi(step)
+
+		if err != nil {
+			panic(err)
+		}
+
+		result += stepChange
+	}
+
+	return result
+}
+
+func DayOneTaskTwo(changes string) int {
+	steps := strings.Split(changes, ", ")
+	freqs := make(map[int]bool, 0)
+
+	result := 0
+	freqs[result] = true
+	for {
+		for _, step := range steps {
+			stepChange, err := strconv.Atoi(step)
+
+			if err != nil {
+				panic(err)
+			}
+
+			result += stepChange
+
+			//log.Printf("Step %s brings us to %d\n", step, result)
+
+			if _, ok := freqs[result]; ok {
+				return result
+			}
+
+			freqs[result] = true
+		}
+	}
 }
